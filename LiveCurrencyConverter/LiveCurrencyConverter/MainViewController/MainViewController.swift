cf47f5f3f222d078d0620976a1fcdb228883a7f0
Sasha Klovak <sanyaklovak@gmail.com>
2024-12-07 12:23:22 +0200

    Initial commit

---
+++ LiveCurrencyConverter/LiveCurrencyConverter/MainViewController/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  LiveCurrencyConverter
+//
+//  Created by Sasha Klovak on 07.12.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
